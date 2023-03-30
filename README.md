22db6b8355872aa1b37040ce3b896efaa8d4ac05
henrique weiand <henriqueweiand@gmail.com>
2023-03-30 20:11:30 -0300

    added base to quizlet integration

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # flash-cards.app-cloud-functions
+
+## How to use
+please inform token Authorization bearer for all requests
+- https://us-central1-flash-cards-app-hw.cloudfunctions.net/app/suggestions/word
+- https://us-central1-flash-cards-app-hw.cloudfunctions.net/app/suggestions/definition
+
+## How to tun
+1. Access `functions`;
+2. Install dependencies;
+3. Running local `npm run serve` (when you do some change it is not refreshing, so you have to delete the folder lib and run again);
+4. To deploy, run `firebase deploy`l
--- functions/src/core/providers/suggestions/index.ts
+++ functions/src/core/providers/suggestions/index.ts
@@ -1,75 +1,87 @@
-export class QuizletSuggestion {
-  private _accounts: string[] = ['-2191095942591596258'];
+import axios from 'axios';
+import {
+  PrefixSuggestionsResponse,
+  WordSuggestionsResponse,
+  IAxiosParams,
+} from './interface';
 
-  getDefinition() {
-    return {
-      responses: [
-        {
-          data: {
-            suggestions: {
-              requestId: '27f37df1-2753-4497-a3b0-3799acbd0c97',
-              parameters: {
-                prefix: '',
-                word: 'love',
-                localTermId: '-1',
-                wordLang: 'en',
-                defLang: 'pt',
-              },
-              suggestions: [
-                {
-                  text: 'amor, amar',
-                  id: 13777302071,
-                },
-                {
-                  text: 'apaixonado',
-                  id: 13777302072,
-                },
-                {
-                  text: 'o amor',
-                  id: 13777302073,
-                },
-                {
-                  text: 'amar, adorar',
-                  id: 13777302074,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    };
+export class QuizletSuggestion {
+  private readonly accounts: string[] = ['-2191095942591596258'];
+  private readonly quizletURL = 'https://quizlet.com/webapi/3.2/suggestions/';
+
+  async getQuizlet({
+    type,
+    wordLang = 'en',
+    defLang = 'pt',
+    prefix = '',
+    word = '',
+  }: {
+    type: 'definition' | 'word';
+    wordLang: string;
+    defLang: string;
+    prefix?: string;
+    word?: string;
+  }): Promise<PrefixSuggestionsResponse | WordSuggestionsResponse | undefined> {
+    let params = {
+      clientId: this.accounts[0],
+      limit: 3,
+      localTermId: '-1',
+      wordLang,
+      defLang,
+      prefix,
+    } as IAxiosParams;
+
+    if (type === 'definition') {
+      params = {
+        ...params,
+        word,
+      };
+    }
+
+    try {
+      const response = await axios.get<
+        PrefixSuggestionsResponse | WordSuggestionsResponse | undefined
+      >(`${this.quizletURL}${type}`, {
+        params,
+      });
+      return response.data;
+    } catch (e) {
+      throw new Error();
+    }
   }
 
-  getWord() {
-    return {
-      responses: [
-        {
-          data: {
-            suggestions: {
-              requestId: '3ce9de6b-b2cd-4c55-b088-00a81f2420e8',
-              parameters: {
-                prefix: 'hear',
-                localTermId: '-1',
-                wordLang: 'en',
-              },
-              suggestions: [
-                {
-                  text: 'heart',
-                  id: 19309032171,
-                },
-                {
-                  text: 'hear',
-                  id: 19309032172,
-                },
-                {
-                  text: 'hearing',
-                  id: 19309032173,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    };
+  async getDefinition() {
+    // word - ?clientId=-2191095942591596258&limit=3&defLang=pt&localTermId=-1&prefix=hear&wordLang=en
+
+    try {
+      const response = await this.getQuizlet({
+        type: 'definition',
+        wordLang: 'en',
+        defLang: 'pt',
+        prefix: '',
+        word: 'love',
+      });
+
+      return response;
+    } catch (e) {
+      throw new Error();
+    }
+  }
+
+  async getWord() {
+    // word - ?clientId=-2191095942591596258&limit=3&defLang=pt&localTermId=-1&prefix=hear&wordLang=en
+
+    try {
+      const response = await this.getQuizlet({
+        type: 'word',
+        wordLang: 'en',
+        defLang: 'pt',
+        prefix: 'hear',
+      });
+
+      return response;
+    } catch (e) {
+      throw new Error();
+    }
   }
 }
